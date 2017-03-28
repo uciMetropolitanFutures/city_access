b2a5072d46035a598ee47aa2592e759a54883d60
Kevin Kane <kevin@kevinkane.org>
2017-03-28 15:59:08 -0700

    Add files via upload

--- server.R
+++ server.R
@@ -2,17 +2,16 @@ library(shiny)
 library(leaflet)
 library(maptools)
 library(sp)
-#setwd("C:/Users/Kevin/Documents/a_UCI/webmap/access")
 
-la <- read.csv("LA_acc_coeff_sig.csv")
-or <- read.csv("OC_acc_coeff_sig.csv")
-rv <- read.csv("RV_acc_coeff_sig.csv")
-sb <- read.csv("SB_acc_coeff_sig.csv")
-vn <- read.csv("VN_acc_coeff_sig.csv")
+la <- read.csv("LA_acc_coeff_sig.csv", stringsAsFactors = F)
+or <- read.csv("OC_acc_coeff_sig.csv", stringsAsFactors = F)
+rv <- read.csv("RV_acc_coeff_sig.csv", stringsAsFactors = F)
+sb <- read.csv("SB_acc_coeff_sig.csv", stringsAsFactors = F)
+vn <- read.csv("VN_acc_coeff_sig.csv", stringsAsFactors = F)
 dest <- read.csv("cat_descr.csv")
 sub <- readShapePoly("SoCal_place_2010_UA")
 dfsub <- data.frame(sub)
-est = c('Apparel Retailing', 'Auto Services', 'Beer, Wine, and Liquor Stores', 'Child Care Services', 'Convenience Stores', 'Deposit-taking Institutions', 'Drinking Places', 'Drug Stores', 'Elementary and Secondary Schools', 'Full-Service Restaurants', 'Gas Stations', 'General Merchandise Retailing', 'Groceries', 'Hair Care Services', 'SKIP', 'Home Products Retailing', 'Hospitals', 'Laundry', 'Limited-Service Food and Beverage', 'Medical Laboratories', 'Open Space', 'Other Learning', 'Other Personal Services', 'Personal Financial', 'Personal Products Retailing', 'Recreational Facilities and Instruction', 'Religious Organizations', 'Repair Services', 'Social Service Organizations', 'Specialty Food', 'Specialty Retailing', 'Rail Stations')
+est = c('Apparel Retailing', 'Auto Services', 'Beer, Wine, and Liquor Stores', 'Child Care Services', 'Convenience Stores', 'Deposit-taking Institutions', 'Drinking Places', 'Drug Stores', 'Elementary and Secondary Schools', 'Full-Service Restaurants', 'Gas Stations', 'General Merchandise Retailing', 'Groceries', 'Hair Care Services', ' ', 'Home Products Retailing', 'Hospitals', 'Laundry', 'Limited-Service Food and Beverage', 'Medical Laboratories', 'Open Space', 'Other Learning', 'Other Personal Services', 'Personal Financial', 'Personal Products Retailing', 'Recreational Facilities and Instruction', 'Religious Organizations', 'Repair Services', 'Social Service Organizations', 'Specialty Food', 'Specialty Retailing')
 
 shinyServer(function(input, output) {
 
@@ -23,96 +22,111 @@ shinyServer(function(input, output) {
     center$y_coord = dfsub$y_coord[dfsub$NAME10==input$cent]
   })
   
-  options = reactiveValues(choose = "et1")
+  options = reactiveValues(choose = "^Avg_1$")
   observeEvent(input$go1, {   
-    link1 = switch(input$topic, "Number of Businesses"="et", "Avg. Number within 1-mile"="Avg_", "Pct. serviced within 1-mile"="Nonzero_")
+    link1 = switch(input$topic, "Total # of Destinations in city"="et", "Abundance"="Avg_", "Percent serviced"="Nonzero_")
     options$choose <- paste("^", link1, grep(input$estabs1, est), "$", sep="")
   })
   
  finalMap <- reactive ({
-    choice = dfsub[,grep(options$choose, colnames(dfsub))]
-    print(options$choose)
-    print(head(choice))
-    print(summary(choice))
-    if(input$topic=="Number of Businesses"){
-      br1 = as.numeric(quantile(choice, 0.25, na.rm=T))+1
+    selected = max("^Avg_1$", options$choose)
+    choice = dfsub[,grep(selected, colnames(dfsub))]
+    if(input$topic=="Total # of Destinations in city"){
+      br1 = as.numeric(quantile(choice[choice>0], 0.25, na.rm=T))
+      br2 = as.numeric(quantile(choice[choice>0], 0.5, na.rm=T))
+      br3 = as.numeric(quantile(choice[choice>0], 0.75, na.rm=T))
+      pal <- colorBin("Blues", choice, bins=c(0, 0.99, br1, br2, br3, as.numeric(summary(choice)[6])), na.color="#B0171F")}
+    else if(input$topic=="Abundance"){
+      br1 = as.numeric(quantile(choice[choice>0], 0.25, na.rm=T))
+      br2 = as.numeric(quantile(choice[choice>0], 0.5, na.rm=T))
+      br3 = as.numeric(quantile(choice[choice>0], 0.75, na.rm=T))
+      pal <- colorBin("Blues", choice, bins=c(0, 0.01, br1, br2, br3, as.numeric(summary(choice)[6])), na.color="#B0171F")}
+    else if(input$topic=="Percent serviced"){
+      br1 = as.numeric(quantile(choice, 0.25, na.rm=T))
       br2 = as.numeric(quantile(choice, 0.5, na.rm=T))
       br3 = as.numeric(quantile(choice, 0.75, na.rm=T))
-      pal <- colorBin("Blues", choice, bins=c(0, br1, br2, br3, as.numeric(summary(choice)[6])), na.color="#B0171F")}
-    else if(input$topic=="Avg. Number within 1-mile"){
-      br1 = as.numeric(quantile(choice, 0.25, na.rm=T))+0.01
-      br2 = as.numeric(quantile(choice, 0.5, na.rm=T))+0.02
-      br3 = as.numeric(quantile(choice, 0.75, na.rm=T))+0.03
-      pal <- colorBin("Blues", choice, bins=c(0, br1, br2, br3, as.numeric(summary(choice)[6])), na.color="#B0171F")}
-    else{
-      br1 = as.numeric(quantile(choice, 0.25, na.rm=T))+0.01
-      br2 = as.numeric(quantile(choice, 0.5, na.rm=T))+0.02
-      br3 = as.numeric(quantile(choice, 0.75, na.rm=T))+0.03
-      pal <- colorBin("Blues", choice, bins=c(0, br1, br2, br3, 1.0), na.color="#B0171F")}
-    
-    #pal <- colorQuantile("Blues", choice, n=5, na.color="#B0171F")
-    m = leaflet(sub) %>% setView(lng=center$x_coord, lat=center$y_coord , zoom=10) %>% addTiles() %>%
+      pal <- colorBin("Blues", choice, bins=c(0, br1, br2, br3, 100), na.color="#B0171F")}
+    m = leaflet(sub) %>% setView(lng=center$x_coord, lat=center$y_coord , zoom=9) %>% addTiles() %>%
       addPolygons(data=sub, stroke=T, weight=1.1, fillColor=~pal(choice), color="black", fillOpacity=0.5,
                   opacity=1, popup=~paste(NAME10)) %>%
-      addLegend("bottomleft", pal=pal, values=~choice, opacity=0.75, na.label=~paste("No", input$estabs1, "in city"),
+      addLegend("bottomleft", pal=pal, values=~choice, opacity=0.75, na.label="Data unavailable",
                 title=paste(input$estabs1, input$topic))
   })
   output$map = renderLeaflet(finalMap())
-  
-  output$data <- renderPrint({
-    dfsub[,grep(paste("^", options$choose, "$", sep=""), colnames(dfsub))][dfsub$NAME10==input$city1]
-  })
-  
+
+  output$hist <- renderPlot({
+        par(mar=c(2.5,4,4,2), oma=c(1.5,0,0,0))
+        selected = max("^Avg_1$", options$choose)  # must 'trick' function into a default selection
+        datause = dfsub[,grep(selected, colnames(dfsub))]
+        datause = datause[!is.na(datause)]
+        lab <- input$estabs1
+        p1 = as.numeric(quantile(datause, 0.01))
+        p99 = as.numeric(quantile(datause, 0.99))
+        hist(datause, xlab=NULL, col="dodgerblue", breaks=((max(datause)-min(datause))/(p99-p1))*12, xlim=c(p1, p99),
+             ylab="# of SoCal Cities", border="white", main=lab, font=2)
+        abline(v=mean(datause), lty=2)
+        val = round(dfsub[,grep(paste("^", selected, "$", sep=""), colnames(dfsub))][dfsub$NAME10==input$city1],2)
+        abline(v=val, lwd=2)
+        legend("topright", c(input$city1), lwd=2, box.col="white")
+        legend("topright", c(input$city1, "Avg"), lwd=c(2,1), lty=c(1,2), box.col="white")
+        mtext(paste(input$topic, " (", input$city1, " = ", val, ")"), side=1, cex=0.85, font=3, outer=TRUE)
+    })
+    
   
   #### HISTOGRAM PANEL ####
-  options = reactiveValues(choose2="b_13", city="", use="") 
+  options = reactiveValues(choose2="b_13", finalcity="Aliso Viejo", use="or") 
   
   observeEvent(input$goLA,{
-    options$choose2 <- paste("b_", grep(input$estabs2, est), sep="")
+    if(grep(input$estabs2, est)==1 | grep(input$estabs2, est)==2 | grep(input$estabs2, est)==3){
+      options$choose <- paste("b_0", grep(input$estabs2, est), sep="")}   # prevents multiple estab types from being selected for 1, 2, and 3
+      else {options$choose2 <- paste("b_", grep(input$estabs2, est), sep="") }  
     options$finalcity = input$cityLA
     options$use = la
   })
   observeEvent(input$goOR,{
-    options$choose2 <- paste("b_", grep(input$estabs2, est), sep="")
+    if(grep(input$estabs2, est)==1 | grep(input$estabs2, est)==2 | grep(input$estabs2, est)==3){
+      options$choose <- paste("b_0", grep(input$estabs2, est), sep="")}   # prevents multiple estab types from being selected for 1, 2, and 3
+    else {options$choose2 <- paste("b_", grep(input$estabs2, est), sep="") }  
     options$finalcity = input$cityOR
     options$use = or
   })
   observeEvent(input$goRV,{
-    options$choose2 <- paste("b_", grep(input$estabs2, est), sep="")
+    if(grep(input$estabs2, est)==1 | grep(input$estabs2, est)==2 | grep(input$estabs2, est)==3){
+      options$choose <- paste("b_0", grep(input$estabs2, est), sep="")}   # prevents multiple estab types from being selected for 1, 2, and 3
+    else {options$choose2 <- paste("b_", grep(input$estabs2, est), sep="") }  
     options$finalcity = input$cityRV
     options$use = rv
   })
   observeEvent(input$goSB,{
-    options$choose2 <- paste("b_", grep(input$estabs2, est), sep="")   
+    if(grep(input$estabs2, est)==1 | grep(input$estabs2, est)==2 | grep(input$estabs2, est)==3){
+      options$choose <- paste("b_0", grep(input$estabs2, est), sep="")}   # prevents multiple estab types from being selected for 1, 2, and 3
+    else {options$choose2 <- paste("b_", grep(input$estabs2, est), sep="") }  
     options$finalcity = input$citySB
     options$use = sb
   })
   observeEvent(input$goVN,{
-    options$choose2 <- paste("b_", grep(input$estabs2, est), sep="")   
+    if(grep(input$estabs2, est)==1 | grep(input$estabs2, est)==2 | grep(input$estabs2, est)==3){
+      options$choose <- paste("b_0", grep(input$estabs2, est), sep="")}   # prevents multiple estab types from being selected for 1, 2, and 3
+    else {options$choose2 <- paste("b_", grep(input$estabs2, est), sep="") }  
     options$finalcity = input$cityVN
     options$use = vn
   })
   
-    #print(options$choose2)
-    #print(dest$descr[dest$name==input$estabs2])
-    
     output$bar <- renderPlot({
       d <- options$use
       c2 = subset(cbind(d$name, d[,grep(options$choose2, colnames(d))]), d$name==options$finalcity)
       c3 = c2[,2:ncol(c2)]
-      barplot(as.numeric(c3), names.arg=substr(colnames(c3),6,20), las=2,  
-              main=paste(options$finalcity, ": ", input$estabs2), xlab="",
-              ylab="Impact of Factors on Accessibility")
+      par(oma=c(3,0,3,0))
+      barplot(as.numeric(c3), names.arg=substr(colnames(c3),6,20), las=2, col="gold",
+              main=paste(options$finalcity, ": ", input$estabs2), xlab="", ylab="Impact of Characteristics on Accessibility")
       abline(h=0)
-      
+      topcatpos <- substr(colnames(c3)[grep(max(c3[c3>0], na.rm=T), c3)],6,20)
+      topcatneg <- substr(colnames(c3)[grep(max(abs(c3[c3<0]), na.rm=T), c3)],6,20)
+      mtext(paste("In", options$finalcity, topcatpos, "has the strongest positive impact on access to", input$estabs2,
+                  "\n while", topcatneg, "has the strongest negative impact."), side=3, cex=0.95, font=3, outer=TRUE)
     })
     output$var_desc <- renderText({
-      print(options$finalcity)
-      print(options$choose2)
       as.character(dest$descr[dest$name==input$estabs2])
     })
-
-
-
 })  
 
--- ui.R
+++ ui.R
@@ -4,21 +4,20 @@ library(maptools)
 library(sp)
 
 # Read in everything
+la <- read.csv("LA_acc_coeff_sig.csv", stringsAsFactors = F)
+or <- read.csv("OC_acc_coeff_sig.csv", stringsAsFactors = F)
+rv <- read.csv("RV_acc_coeff_sig.csv", stringsAsFactors = F)
+sb <- read.csv("SB_acc_coeff_sig.csv", stringsAsFactors = F)
+vn <- read.csv("VN_acc_coeff_sig.csv", stringsAsFactors = F)
 dest <- read.csv("cat_descr.csv", stringsAsFactors = F)
-la <- read.csv("LA_acc_coeff_sig.csv")
-or <- read.csv("OC_acc_coeff_sig.csv")
-rv <- read.csv("RV_acc_coeff_sig.csv")
-sb <- read.csv("SB_acc_coeff_sig.csv")
-vn <- read.csv("VN_acc_coeff_sig.csv")
-
 sub <- readShapePoly("SoCal_place_2010_UA")
 dfsub <- data.frame(sub)
 names = as.character(unique(unlist(dfsub$NAME10)))
-est = c('Apparel Retailing', 'Auto Services', 'Beer, Wine, and Liquor Stores', 'Child Care Services', 'Convenience Stores', 'Deposit-taking Institutions', 'Drinking Places', 'Drug Stores', 'Elementary and Secondary Schools', 'Full-Service Restaurants', 'Gas Stations', 'General Merchandise Retailing', 'Groceries', 'Hair Care Services', 'SKIP', 'Home Products Retailing', 'Hospitals', 'Laundry', 'Limited-Service Food and Beverage', 'Medical Laboratories', 'Open Space', 'Other Learning', 'Other Personal Services', 'Personal Financial', 'Personal Products Retailing', 'Recreational Facilities and Instruction', 'Religious Organizations', 'Repair Services', 'Social Service Organizations', 'Specialty Food', 'Specialty Retailing', 'Rail Stations')
+est = c('Apparel Retailing', 'Auto Services', 'Beer, Wine, and Liquor Stores', 'Child Care Services', 'Convenience Stores', 'Deposit-taking Institutions', 'Drinking Places', 'Drug Stores', 'Elementary and Secondary Schools', 'Full-Service Restaurants', 'Gas Stations', 'General Merchandise Retailing', 'Groceries', 'Hair Care Services', ' ', 'Home Products Retailing', 'Hospitals', 'Laundry', 'Limited-Service Food and Beverage', 'Medical Laboratories', 'Open Space', 'Other Learning', 'Other Personal Services', 'Personal Financial', 'Personal Products Retailing', 'Recreational Facilities and Instruction', 'Religious Organizations', 'Repair Services', 'Social Service Organizations', 'Specialty Food', 'Specialty Retailing')
 
-shinyUI(navbarPage("Accessibility to Retail and Services in SoCal Cities.", id="nav",
+shinyUI(navbarPage("Accessibility to everyday destinations in SoCal Cities.", id="nav",
   
-  tabPanel("Proximity Across Cities", div(class="outer",
+  tabPanel("Access Across Cities", div(class="outer",
           tags$head(includeCSS("styles.css")),
           leafletOutput("map", width="100%", height="100%"),
           absolutePanel(id="controls", class="panel panel-default", fixed=T, draggable=T, 
@@ -26,54 +25,58 @@ shinyUI(navbarPage("Accessibility to Retail and Services in SoCal Cities.", id="
                         selectInput("cent", label=strong("Zoom to City:"), selected="Irvine", choices=names[order(names)]),
                         actionButton("recenter", label="Re-center map"),
                         br(""),
-                        radioButtons("topic", label=strong("Select Topic:"), selected="Number of Businesses",
-                                     choices = c("Number of Businesses", "Avg. Number within 1-mile", "Pct. serviced within 1-mile")),
-                        selectInput("estabs1", label=strong("Select Estabs:"), choices=est, selected=""),
-                        actionButton("go1", label="Go/Refresh"),
-                        selectInput("city1", label=strong("Show value for city:"), selected="Agoura Hills", choices=names[order(names)]),
-                        verbatimTextOutput("data")),
+                        radioButtons("topic", label=strong("Select Measure:"), selected="Abundance",
+                                     choices = c("Abundance", "Percent serviced", "Total # of Destinations in city")),
+                        selectInput("estabs1", label=strong("Select Destination Type:"), choices=est, selected="Apparel Retailing"),
+                        actionButton("go1", label=strong("Click to Refresh after re-selecting", style="color:red")),
+                        selectInput("city1", label=strong("Select city for details:"), selected="Agoura Hills", choices=names[order(names)]),
+                        plotOutput("hist", height=235)),
           absolutePanel(id="controls", class="panel panel-default", fixed=T, draggable=T,
-                        top=110, left=10, right="auto", bottom="auto", width=160, height="auto",
-                        p("Data notes:"),
-                        h6("Please be patient while map loads!"),
-                        h6("This is..."),
-                        h6("That is..."),
-                        h6("-- See", a("our website", href="http://mfi.soceco.uci.edu", target="_blank"), "for details."))
+                        top=110, left=10, right="auto", bottom="auto", width=175, height="auto",
+                        p(strong("Data notes:")),
+                        h6("-- This map shows how accessible homes in a city are to a variety of everyday destinations."),
+                        h6("-- ", strong("Abundance"), "shows how many of that destination type are within one mile of the average home in the selected city."),
+                        h6("-- ", strong("Percent serviced"), "displays the % of homes in a city which have at least one of that destination in one mile."),
+                        h6("-- All distances are calculated along a street network."),
+                        h6("-- 1/5 of the region's cities are each color on the map."),
+                        h6("-- Make sure to click Refresh after changing the selection."),
+                        h6("-- By the UCI", a("Metropolitan Futures Initiative.", href="http://mfi.soceco.uci.edu", target="_blank"),
+                           " See", a("the full report here.", href="http://mfi.soceco.uci.edu/category/quarterly-report/", target="_blank")))
           )),
 
   
-  
-  tabPanel("What Impacts Proximity?",
+  tabPanel("What Impacts Access?",
   sidebarLayout(
     sidebarPanel(
-      selectInput("estabs2", label=strong("Select Store Type"), choices=est, selected="Groceries"),
-      radioButtons("county", label=strong("Select County"), selected=2,
+      selectInput("estabs2", label=strong("Select Destination Type:", style="color:blue"), choices=est, selected="Groceries"),
+      radioButtons("county", label=strong("Select County:", style="color:blue"), selected=2, 
                    choices = list("Los Angeles"=1, "Orange"=2, "Riverside"=3, "San Bernardino"=4, "Ventura"=5)),
       conditionalPanel("input.county == 1",
-                       selectInput("cityLA", label=strong("Select City"), selected="", choices=as.character(la$name)[order(as.character(la$name))]),
-                       actionButton("goLA", label="Go/Refresh")),
+                       selectInput("cityLA", label=strong("Select City:", style="color:blue"), selected="", choices=as.character(la$name)[order(as.character(la$name))]),
+                       actionButton("goLA", label=strong("Click to Start & to Refresh", style="color:red"))),
       conditionalPanel("input.county == 2",
-                       selectInput("cityOR", label=strong("Select City"), selected="", choices=as.character(or$name)[order(as.character(or$name))]),
-                       actionButton("goOR", label="Go/Refresh")),
+                       selectInput("cityOR", label=strong("Select City:", style="color:blue"), selected="Aliso Viejo", choices=as.character(or$name)[order(as.character(or$name))]),
+                       actionButton("goOR", label=strong("Click to Start & to Refresh", style="color:red"))),
       conditionalPanel("input.county == 3",
-                       selectInput("cityRV", label=strong("Select City"), selected="", choices=as.character(rv$name)[order(as.character(rv$name))]),
-                       actionButton("goRV", label="Go/Refresh")),
+                       selectInput("cityRV", label=strong("Select City:", style="color:blue"), selected="", choices=as.character(rv$name)[order(as.character(rv$name))]),
+                       actionButton("goRV", label=strong("Click to Start & to Refresh", style="color:red"))),
       conditionalPanel("input.county == 4",
-                       selectInput("citySB", label=strong("Select City"), selected="", choices=as.character(sb$name)[order(as.character(sb$name))]),
-                       actionButton("goSB", label="Go/Refresh")),
+                       selectInput("citySB", label=strong("Select City:", style="color:blue"), selected="", choices=as.character(sb$name)[order(as.character(sb$name))]),
+                       actionButton("goSB", label=strong("Click to Start & to Refresh", style="color:red"))),
       conditionalPanel("input.county == 5",
-                       selectInput("cityVN", label=strong("Select City"), selected="", choices=as.character(vn$name)[order(as.character(vn$name))]),
-                       actionButton("goVN", label="Go/Refresh")),
+                       selectInput("cityVN", label=strong("Select City:", style="color:blue"), selected="", choices=as.character(vn$name)[order(as.character(vn$name))]),
+                       actionButton("goVN", label=strong("Click to Start & to Refresh", style="color:red"))),
       br(), 
-      p(strong("Data Notes:")),
+      p(strong("Destination Type Notes:", style="color:blue")),
       textOutput("var_desc"),
       br(),
-      a("UCI Metropolitan Futures Initiative", href="http://mfi.soceco.uci.edu"),
-      img(src="mfi.png", height=200, width=200)),
+      h6("-- By the UCI", a("Metropolitan Futures Initiative.", href="http://mfi.soceco.uci.edu", target="_blank"),
+         " See", a("the full report here.", href="http://mfi.soceco.uci.edu/category/quarterly-report/", target="_blank"))),
     mainPanel(
-      h2("What Impacts How Close Things Are to Me?"),
-      p(em("Factors which influence accessibility are displayed below. ")),   #em is used for italics
-      plotOutput("bar", height = 400))
+      h2(strong("What Impacts How Close Things Are to Me?", style="color:blue")),
+      h6(em("-- The bars below correspond to characteristics of the ", strong("home ", style="color:blue"), "or the ", strong("neighborhood.", style="color:blue"), "For housing types, e.g. Condo, we show the effect of Condo status on access versus a detached, single-family home.")),
+      h6(em("-- The height of each bar shows how much impact that characteristic has on accessibility to the chosen destination.")),
+      plotOutput("bar", height = 500))
     ))
 ))
     
